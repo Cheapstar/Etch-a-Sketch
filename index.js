eded8151b91e1a281a0a5bca33dedec9c317a519
PRASANG <cheap@gmail.com>
2023-11-26 05:57:50 +0530

    Add a User interface
    Added a button which takes user input for grid and create the grid accordingly

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
+const body=document.querySelector("body");
+body.style.display = "flex";
+body.style.justifyContent = "center";
+
+
+
+//Input Button
+const button = document.createElement("button");
+button.textContent = "Create";
+body.appendChild(button);
+button.addEventListener("click",()=>{
+    grid.innerHTML="";
+    let input = 101;  
+    while(input>100){
+        input = Number(prompt("Enter the number of squares(<100) : "));
+    };
+    createGrid(input);
+});
+
+//Main Container
 const grid = document.createElement("div");
 grid.style.height="700px";
 grid.style.width="700px";
 grid.style.display="flex";
 grid.style.flexWrap="wrap";
 
-const body=document.querySelector("body");
 body.appendChild(grid);
 
 
-
-let gridItems = new Array(256);
-
-for(let i=0;i<256;i++){
-    gridItems[i] = document.createElement("div");
-    gridItems[i].style.width = "41.75px";
-    gridItems[i].style.height = "41.75px";
-    gridItems[i].style.border = "1px solid black"
-    grid.appendChild(gridItems[i]);
-}
-
 const gridColors = ["aqua", "black", "blue", "fuchsia", "gray", "green", "lime", "maroon", "navy", "olive", "purple", "red", "silver", "teal", "white", "yellow"];
 
-grid.addEventListener("mouseover",(event)=>{
-    event.target.style.backgroundColor = gridColors[Math.floor((Math.random()) * 16)];
-});
-grid.addEventListener("click",(event)=>{
-    event.target.style.backgroundColor = "";
-})
+//creates nxn grid as per the user
+function createGrid(input){
+
+    let gridItems = new Array(input*input);
+
+    for(let i=0;i<(input*input);i++){
+        gridItems[i] = document.createElement("div");
+        gridItems[i].style.width = `${Number((700/input).toFixed(2))}px`;
+        gridItems[i].style.height = `${Number((700/input).toFixed(2))}px`;
+        gridItems[i].style.boxSizing = "border-box";
+        gridItems[i].style.border = "1px solid black"
+        grid.appendChild(gridItems[i]);
+        gridItems[i].addEventListener("mouseover",(event)=>{
+            event.target.style.backgroundColor = gridColors[Math.floor((Math.random()) * 16)];
+        });
+        gridItems[i].addEventListener("click",(event)=>{
+            event.target.style.backgroundColor = "";
+        });
+    }
+}
+
+
+
+createGrid(16);
